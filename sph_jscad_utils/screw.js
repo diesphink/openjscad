3bfc2a6a5b36767d82e5d7c74bbbad27c3c36da9
Diego Pereyra <diesphink@gmail.com>
2021-07-12 09:42:48 -0300

    parafuso com opção sem cabeça ou com cabeça cilíndrica

--- sph_jscad_utils/screw.js
+++ sph_jscad_utils/screw.js
@@ -1,23 +1,38 @@
-const { cylinder, cylinderElliptic } = require('@jscad/modeling/src/primitives');
-const { union } = require('@jscad/modeling/src/operations/booleans');
-const { align } = require("./align.js")
+const { cylinder, cylinderElliptic } = require("@jscad/modeling/src/primitives");
+const { union } = require("@jscad/modeling/src/operations/booleans");
+const { align } = require("./align.js");
 
-const STYLE_CONE = 0
+const STYLE_CONE = 0;
+const STYLE_CYLINDER = 1;
 
-const screw = (diameter, length, { head_length = 3, head_diameter = 8, head_style = STYLE_CONE } = {}) => {
+const screw = (diameter, length, { head_length = 0, head_diameter = 8, head_style = STYLE_CONE } = {}) => {
+  const body = cylinder({ height: length - head_length, radius: diameter / 2 });
 
-    const body = cylinder({ height: length - head_length, radius: diameter / 2 })
-    const head = align(
-        cylinderElliptic({
-            height: head_length,
-            startRadius: [diameter / 2, diameter / 2],
-            endRadius: [head_diameter / 2, head_diameter / 2]
-        }), {
+  if (head_length == 0) return body;
+
+  let head;
+
+  if (head_style == STYLE_CONE) {
+    head = align(
+      cylinderElliptic({
+        height: head_length,
+        startRadius: [diameter / 2, diameter / 2],
+        endRadius: [head_diameter / 2, head_diameter / 2],
+      }),
+      {
         ref: body,
-        center: 'xy',
-        beginToEnd: 'z'
-    })
-    return union(body, head)
-}
+        center: "xy",
+        beginToEnd: "z",
+      }
+    );
+  } else if (head_style == STYLE_CYLINDER) {
+    head = align(cylinder({ height: head_length, radius: head_diameter/2 }), {
+      ref: body,
+      center: "xy",
+      beginToEnd: "z",
+    });
+  }
+  return union(body, head);
+};
 
-module.exports = { screw, STYLE_CONE }
+module.exports = { screw, STYLE_CONE, STYLE_CYLINDER };
